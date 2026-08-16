1f3602112efd54679b92183d66a3e42801c0327f
derickmokua <derickmokua@outlook.com>
2026-08-16 15:44:44 +0300

    Optimize JS payload sizes and evaluation via Framer Motion lazy loading and modern browserslist

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import { ChatProvider } from "./context/ChatContext";
-import { LazyMotion, domAnimation } from "framer-motion";
+import { LazyMotion } from "framer-motion";
+
+const loadFeatures = () => import("./features").then(res => res.default);
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <LazyMotion features={domAnimation}>
+        <LazyMotion features={loadFeatures} strict>
             <ChatProvider>{children}</ChatProvider>
         </LazyMotion>
     );
